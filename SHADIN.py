c57751af18ccfcfce6a2ccd272232ec7413d0605
SHADIN-143 <148645585+SHADIN-143@users.noreply.github.com>
2023-12-30 10:16:08 +0600

    Update SHADIN.py

--- SHADIN.py
+++ SHADIN.py
@@ -1,15 +1,4 @@
 # Owner ● SHADIN-143
-import os,time,platform
-import os, platform, time, sys
-os.system('clear')
-print(' [\x1b[38;5;48m●\033[1;37m]\x1b[38;5;46m CHECKING UPDATE...!\033[1;37m [\x1b[38;5;48m–\033[1;37m]\033[1;37m')
-time.sleep(5)
-os.system('clear')
-print('\033[1;97m[\x1b[38;5;48m●\033[1;37m] \x1b[38;5;50mJOIN MY PUBLIC GROUP ')
-time.sleep(0.1)
-os.system(f'xdg-open https://facebook.com/groups/354277300393287/')
-os.system('clear')
-print('\x1b[38;5;46mLoding...\x1b[38;5;197m[\x1b[38;5;46mSHADIN-143\x1b[38;5;197m]\x1b[38;5;46m')
 os.system('git pull')
 os.system('pip uninstall requests -y');os.system('pip install requests')
 bit = platform.architecture()[0]
