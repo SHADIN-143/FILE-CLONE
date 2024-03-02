20a7fe9b01c917fffe6fa63a1bf78d82a7cf8da7
SHADIN-143 <148645585+SHADIN-143@users.noreply.github.com>
2024-03-02 09:24:17 +0600

    Update SHADIN.py

--- SHADIN.py
+++ SHADIN.py
@@ -1,6 +1,5 @@
 import os,time,platform
 os.system('git pull')
-os.system('pip uninstall requests -y');os.system('pip install requests')
 bit = platform.architecture()[0]
 if bit=='64bit':
     import SHADIN
